3e9b8c97be7c3fe20731652c0013510774a5bf27
OPWonShinobi <jxia11@my.bcit.ca>
2018-02-08 15:14:34 -0800

    added placeholder img to homepage

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,13 +1,24 @@
 <h1>Welcome to CodeIgniter!</h1>
 
 <div id="body">
+	<div>
+		<summary>Premade sets</summary>
+		<select>
+		  <option value="black">car</option>
+		  <option value="white">car</option>
+		  <option value="asian">Chinese</option>
+		  <option value="brunette">car</option>
+		</select>
+	</div>
+	<img src="/assets/img/black-man-hi.png" default="person">
+
 	<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
 
 	<p>If you would like to edit this page you'll find it located at:</p>
 	<code>application/views/welcome_message.php</code>
 
 	<p>The corresponding controller for this page is found at:</p>
-	<code>application/controllers/Welcome.php</code>
+	<code>testing</code>
 
 	<p>If you are exploring CodeIgniter for the very first time, you should 
 		start by reading the <a href="http://www.codeigniter.com/user_guide/">User Guide</a>.</p>
