37b0e0c280fd564820e05928a5ad56ee5d15073a
rogerzhang18 <rogeryj.zhang@gmail.com>
2018-02-09 09:05:21 -0800

    added mainpage and maintemplate start file
    Signed-off-by: rogerzhang18 <rogeryj.zhang@gmail.com>

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -23,7 +23,7 @@ class Application extends CI_Controller
 
 		//  Set basic view parameters
 		$this->data = array ();
-		$this->data['pagetitle'] = 'CodeIgniter3.1 Starter 4';
+		$this->data['pagetitle'] = 'Path of Exile';
 		$this->data['ci_version'] = (ENVIRONMENT === 'development') ? 'CodeIgniter Version <strong>'.CI_VERSION.'</strong>' : '';
 	}
 
---
+++ application/views/homepage.php
@@ -0,0 +1,10 @@
+<div class="POE_main">
+	<div> 
+		<h1 id="homepage-title">Welcome to Path of Exile custom mod!</h1>
+		<div class="homepage-content">
+			<p> This is a customized <strong>Path of Exile</strong> custom equipment build.</p>
+			<p> The project consists of weapons and armors for the user to choose and build.</p>
+			<p> We are dedicated provide the best gaming experience for you!</p>
+		</div>
+	</div>
+</div>
