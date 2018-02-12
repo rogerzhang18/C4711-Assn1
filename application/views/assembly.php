d2a71a8587461f8b16067b9c211bbf29d0063a1c
Alex Xia <xiaalex1998@gmail.com>
2018-02-11 18:22:09 -0800

    Revert "new Dropdown and js"

--- application/views/assembly.php
+++ application/views/assembly.php
@@ -1,18 +1,11 @@
 <div class="POE_assembly">
-    <h1 id="assembly-title">Equipment - Path of Exile Inventory Simulator</h1>
+    <h1 id="assembly-title">Assembly - Path of Exile custom mod</h1>
     <div class="assembly-content">
-        <p>This is the{page_category} page.</p>
+        <p> This is the Assembly page.</p>
     </div>
     <div class="row">
- 	{items}
- 		<div class="col-xs-4">
- 			<img class="img-responsive" src="/assets/img/{id}.png" title="{name}">
- 			<p>item: {name}</p>
- 			<p>category: {category}</p>
- 			<p>strength: {str}</p>
- 			<p>dexterity: {dex}</p>
- 			<p>intelligence: {int}</p>
- 		</div>
- 	{/items}
+ 	{parts}
+ 		<div class="col-xs-4"><img class="img-responsive" src="/assets/img/{name}.png" title="{name}"></div>
+ 	{/parts}
     </div> 
 </div>
--- application/views/homepage.php
+++ application/views/homepage.php
@@ -7,19 +7,17 @@
 	<div> 
 		<h1 id="homepage-title">Welcome to Path of Exile custom mod!</h1>
 		<div class="homepage-content">
-
-        <div id="preset-dropdown">
-        <select class="round" name="preset-dropdown" onchange="replaceImage(this);">
-            {parts}
-              <option value={name}>{name}</option>
-            {/parts}
-        </select>
-        </div>
-        <img id="base-image" src="./assets/img/background.png" />
-        <p> This is a customized <strong>Path of Exile</strong> custom equipment build.</p>
-        <p> The project consists of weapons and armors for the user to choose and build.</p>
-        <p> We are dedicated provide the best gaming experience for you!</p>
-
+                    <div id="preset-dropdown">
+                    <select class="round" name="preset-dropdown" onchange="replaceImage(this);">
+                        {parts}
+                          <option value={name}>{name}</option>
+                        {/parts}
+                    </select>
+                    </div>
+                    <img id="base-image" src="./assets/img/background.png" />
+                    <p> This is a customized <strong>Path of Exile</strong> custom equipment build.</p>
+                    <p> The project consists of weapons and armors for the user to choose and build.</p>
+                    <p> We are dedicated provide the best gaming experience for you!</p>
 		</div>
 	</div>
 </div>
